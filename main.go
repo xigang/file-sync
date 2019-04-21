6513f14dcd20db6fc99ad0260872fadb07ef0211
xigang <wangxigang2014@gmail.com>
2019-04-21 14:19:27 +0800

    add sync-file tools

---
+++ main.go
@@ -0,0 +1,105 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+
+	"github.com/fsnotify/fsnotify"
+	"github.com/golang/glog"
+)
+
+var (
+	filename             string
+	username             string
+	password             string
+	sshKey               string
+	hostname             string
+	remotePath           string
+	remotePathPermission string
+)
+
+func init() {
+	flag.StringVar(&filename, "filename", "", "watcher file name")
+	flag.StringVar(&username, "username", "", "ssh username")
+	flag.StringVar(&password, "password", "", "ssh password")
+	flag.StringVar(&sshKey, "ssh-key", "", "ssh key filename")
+	flag.StringVar(&hostname, "hostname", "", "ssh remote hostname")
+	flag.StringVar(&remotePath, "remote-path", "/", "copy the path to the remote server")
+	flag.StringVar(&remotePathPermission, "remote-path-permission", "0655", "copy the path permission to the remote server")
+}
+
+func main() {
+	flag.Parse()
+
+	var err error
+	if err = flagParamsValid(); err != nil {
+		glog.Errorf("%+v", err)
+		return
+	}
+
+	config := SSHConfig{
+		username: username,
+		password: password,
+		sshKey:   sshKey,
+		hostname: hostname,
+	}
+	fileSyncManager := NewFileSyncManager(filename, config)
+
+	var watcher *fsnotify.Watcher
+	watcher, err = fsnotify.NewWatcher()
+	if err != nil {
+		glog.Errorf("failed to init watcher: %v\n", err)
+		return
+	}
+	defer watcher.Close()
+	fmt.Println(filename)
+
+	err = watcher.Add(filename)
+	if err != nil {
+		glog.Errorf("failed to add watch filename: %v\n", filename)
+		return
+	}
+
+	done := make(chan bool)
+	go func() {
+		for {
+			select {
+			case event, ok := <-watcher.Events:
+				if !ok {
+					return
+				}
+
+				if event.Op&fsnotify.Write == fsnotify.Write {
+					fileSyncManager.SyncFile(event.Name)
+				}
+			case err, ok := <-watcher.Errors:
+				if !ok {
+					return
+				}
+				glog.Errorf("failed to watcher error: %v\n", err)
+			}
+		}
+	}()
+
+	<-done
+}
+
+func flagParamsValid() error {
+	if filename == "" {
+		return fmt.Errorf("filename is empty, please specify a filename")
+	}
+
+	if username == "" {
+		return fmt.Errorf("username is empty, please specify a username")
+	}
+
+	if hostname == "" {
+		return fmt.Errorf("hostname is empty, please specity a remote hostname")
+	}
+
+	if password == "" && sshKey == "" {
+		return fmt.Errorf("password or ssh-key is empty, please specify password or ssh-key")
+	}
+
+	return nil
+}
---
+++ sync.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/bramvdbogaerde/go-scp"
+	"github.com/bramvdbogaerde/go-scp/auth"
+	"golang.org/x/crypto/ssh"
+)
+
+type SSHConfig struct {
+	username string
+	password string
+	sshKey   string
+	hostname string
+}
+
+type FileSyncManager struct {
+	filename             string
+	remotePath           string
+	remotePathPermission string
+	sshConfig            SSHConfig
+}
+
+func NewFileSyncManager(filename string, config SSHConfig) *FileSyncManager {
+	return &FileSyncManager{
+		filename: filename,
+
+		sshConfig: config,
+	}
+}
+
+func (fs *FileSyncManager) SyncFile(filename string) error {
+	var (
+		clientConfig ssh.ClientConfig
+		err          error
+	)
+	if fs.sshConfig.sshKey != "" {
+		clientConfig, err = auth.PrivateKey(fs.sshConfig.username, fs.sshConfig.sshKey, ssh.InsecureIgnoreHostKey())
+	} else if fs.sshConfig.password != "" {
+		clientConfig, err = auth.PasswordKey(fs.sshConfig.username, fs.sshConfig.password, ssh.InsecureIgnoreHostKey())
+	} else {
+		return fmt.Errorf("params invalid")
+	}
+
+	//create a new SCP client
+	remoteSSHDAddr := fmt.Sprintf("%s:22", fs.sshConfig.hostname)
+	client := scp.NewClient(remoteSSHDAddr, &clientConfig)
+
+	//connect to the remote server
+	if err = client.Connect(); err != nil {
+		return fmt.Errorf("Couldn't establisch a connection to the remote server ", err)
+	}
+	defer client.Close()
+
+	//open sync file
+	var f *os.File
+	if f, err = os.Open(fs.filename); err != nil {
+		return err
+	}
+	defer f.Close()
+
+	//finaly, copy the file over
+	if err = client.CopyFile(f, fs.remotePath, fs.remotePathPermission); err != nil {
+		return err
+	}
+
+	return nil
+}
